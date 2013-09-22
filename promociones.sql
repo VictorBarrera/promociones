447845f0c4ef3d3d5de85fc06113ebf73c3a9fb4
VictorBarrera <vrbh85@gmail.com>
2013-09-22 00:48:58 -0600

    actualizando bd promociones

--- promociones.sql
+++ promociones.sql
@@ -1,32 +1,62 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.32, for Win32 (x86)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 19-09-2013 a las 22:01:06
--- Versión del servidor: 5.5.27
--- Versión de PHP: 5.4.7
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: promociones
+-- ------------------------------------------------------
+-- Server version	5.5.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `promociones`
+-- Current Database: `promociones`
 --
 
--- --------------------------------------------------------
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `promociones` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `promociones`;
 
 --
--- Estructura de tabla para la tabla `clientes`
+-- Table structure for table `categorias`
 --
 
-CREATE TABLE IF NOT EXISTS `clientes` (
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` char(1) DEFAULT NULL,
+  `valor_1` decimal(10,2) DEFAULT NULL,
+  `valor_2` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'c',1.00,100.00),(2,'b',101.00,1000.00),(3,'a',1001.00,1000000.00);
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
   `idcliente` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(50) DEFAULT NULL,
   `departamento` varchar(20) DEFAULT NULL,
@@ -34,141 +64,153 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `feha_nacimiento` date DEFAULT NULL,
   `sexo` char(1) DEFAULT '1',
   PRIMARY KEY (`idcliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `productos`
+-- Dumping data for table `clientes`
 --
 
-CREATE TABLE IF NOT EXISTS `productos` (
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productos` (
   `idproducto` int(11) NOT NULL DEFAULT '0',
   `descripcion` varchar(50) DEFAULT NULL,
   `precio` double DEFAULT NULL,
   PRIMARY KEY (`idproducto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `productos`
+-- Dumping data for table `productos`
 --
 
-INSERT INTO `productos` (`idproducto`, `descripcion`, `precio`) VALUES
-(1, 'televisor', 150),
-(2, 'refrigeradora', 250),
-(3, 'cocina', 260),
-(4, 'celular', 540),
-(5, 'lavadora', 369),
-(6, 'plancha', 26),
-(7, 'vestido', 78),
-(8, 'zapatos', 150),
-(9, 'play station 4', 684),
-(10, 'nintendo wii', 541),
-(11, 'camara digital', 210),
-(12, 'cama', 126),
-(13, 'secadora de pelo', 125),
-(14, 'comedor', 561),
-(15, 'bateria de cocina', 150),
-(16, 'pantalla lcd', 698),
-(17, 'laptop', 840),
-(18, 'computadora destok', 459),
-(19, 'mouse', 12),
-(20, 'monitor lcd', 125),
-(21, 'discoduro externo', 100),
-(22, 'edredon', 156),
-(23, 'juguetera', 258),
-(24, 'licuadora', 98),
-(25, 'chinero', 145),
-(26, 'ventilador', 80),
-(27, 'aire acondicionado', 269),
-(28, 'mueble de pc', 35),
-(29, 'dvd', 50),
-(30, 'ropero', 400);
-
--- --------------------------------------------------------
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'televisor',150),(2,'refrigeradora',250),(3,'cocina',260),(4,'celular',540),(5,'lavadora',369),(6,'plancha',26),(7,'vestido',78),(8,'zapatos',150),(9,'play station 4',684),(10,'nintendo wii',541),(11,'camara digital',210),(12,'cama',126),(13,'secadora de pelo',125),(14,'comedor',561),(15,'bateria de cocina',150),(16,'pantalla lcd',698),(17,'laptop',840),(18,'computadora destok',459),(19,'mouse',12),(20,'monitor lcd',125),(21,'discoduro externo',100),(22,'edredon',156),(23,'juguetera',258),(24,'licuadora',98),(25,'chinero',145),(26,'ventilador',80),(27,'aire acondicionado',269),(28,'mueble de pc',35),(29,'dvd',50),(30,'ropero',400);
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `promociones`
+-- Table structure for table `promociones`
 --
 
-CREATE TABLE IF NOT EXISTS `promociones` (
+DROP TABLE IF EXISTS `promociones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `promociones` (
   `idpromociones` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_promo` varchar(100) DEFAULT NULL,
   `fecha_desde` date DEFAULT NULL,
   `fecha_hasta` date DEFAULT NULL,
   `descripcion` text CHARACTER SET latin1,
   PRIMARY KEY (`idpromociones`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `promociones`
+-- Dumping data for table `promociones`
 --
 
-INSERT INTO `promociones` (`idpromociones`, `nombre_promo`, `fecha_desde`, `fecha_hasta`, `descripcion`) VALUES
-(1, 'Cupones de descuento', '2012-01-12', '2012-02-12', 'Por cada compra mayor a $10 se le dar?í un cup??n al cliente para que lo canjee por la mierda que ?®l quiera.'),
-(2, 'Comida gratis', '2013-05-12', '2013-05-12', 'Por cada compra mayor de $100 se les dar?í un vale de pollo campero para que se vayan hartar gratis.');
-
--- --------------------------------------------------------
+LOCK TABLES `promociones` WRITE;
+/*!40000 ALTER TABLE `promociones` DISABLE KEYS */;
+INSERT INTO `promociones` VALUES (1,'Cupones de descuento','2012-01-12','2012-02-12','Por cada compra mayor a $10 se le dar?í un cup??n al cliente para que lo canjee por la mierda que ?®l quiera.'),(2,'Comida gratis','2013-05-12','2013-05-12','Por cada compra mayor de $100 se les dar?í un vale de pollo campero para que se vayan hartar gratis.');
+/*!40000 ALTER TABLE `promociones` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `search_terms`
+-- Table structure for table `search_terms`
 --
 
-CREATE TABLE IF NOT EXISTS `search_terms` (
+DROP TABLE IF EXISTS `search_terms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `search_terms` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `term` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `search_terms`
+-- Dumping data for table `search_terms`
 --
 
-INSERT INTO `search_terms` (`id`, `term`) VALUES
-(1, 'amor');
-
--- --------------------------------------------------------
+LOCK TABLES `search_terms` WRITE;
+/*!40000 ALTER TABLE `search_terms` DISABLE KEYS */;
+INSERT INTO `search_terms` VALUES (1,'amor');
+/*!40000 ALTER TABLE `search_terms` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `tweets`
+-- Table structure for table `tweets`
 --
 
-CREATE TABLE IF NOT EXISTS `tweets` (
+DROP TABLE IF EXISTS `tweets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweets` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `tweet_id` text NOT NULL,
   `user_id` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Dumping data for table `tweets`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
+LOCK TABLES `tweets` WRITE;
+/*!40000 ALTER TABLE `tweets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tweets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `passwd` varchar(50) NOT NULL,
   `tipo` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `passwd`, `tipo`) VALUES
-(1, 'Rafael Parada', 'rafa', '12345', 'admin');
-
--- --------------------------------------------------------
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Rafael Parada','rafa','12345','admin');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `ventas`
+-- Table structure for table `ventas`
 --
 
-CREATE TABLE IF NOT EXISTS `ventas` (
+DROP TABLE IF EXISTS `ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ventas` (
   `idventa` int(11) NOT NULL DEFAULT '0',
   `idproducto` int(11) DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL,
@@ -176,7 +218,24 @@ CREATE TABLE IF NOT EXISTS `ventas` (
   `fechaventa` date DEFAULT NULL,
   PRIMARY KEY (`idventa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `ventas`
+--
+
+LOCK TABLES `ventas` WRITE;
+/*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-09-22  0:47:42
